6f91960d6f9bca5d61194e8ce6a8052577b81944
felipebaguena <felipebaguena@gmail.com>
2023-04-10 13:42:05 +0200

    feat: first commit and roles/heroes migrations

---
+++ bbdd_final/database/migrations/2023_04_10_113603_create_heroes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->string('name');
+            $table->string('story');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->integer('health');
+            $table->integer('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
