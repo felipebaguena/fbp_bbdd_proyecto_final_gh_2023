fbbb2f35a01b9a79689fedc9d9808979fae51396
felipebaguena <felipebaguena@gmail.com>
2023-04-10 17:41:32 +0200

    feat: battles table migration

---
+++ bbdd_final/database/migrations/2023_04_10_153733_create_battles_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('battles', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('hero_id');
+            $table->foreign('hero_id')
+            ->references('id')
+            ->on('heroes')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('monster_id');
+            $table->foreign('monster_id')
+            ->references('id')
+            ->on('monsters')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('stage_id');
+            $table->foreign('stage_id')
+            ->references('id')
+            ->on('stages')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('battles');
+    }
+};
